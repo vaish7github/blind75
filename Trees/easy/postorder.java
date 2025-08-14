f88748e65e711d5b87a9f38cf1bc09c55c09f161
vaish7github <vaishnavitiwari968@gmail.com>
2025-08-14 16:19:05 +0530

    post order

--- Trees/easy/postorder.java
+++ Trees/easy/postorder.java
@@ -1,40 +1,42 @@
 import java.util.*;
+
 class main{
-    static class TreeNode {
+    static class TreeNode{
         int data;
         TreeNode left;
         TreeNode right;
         TreeNode(int x){
             data= x;
             left= null;
-            right=null;
+            right = null;
         }
+
     }
+
     public static void main(String [] args){
         Scanner sc= new Scanner(System.in);
-        int n= sc.nextInt();
-        int[] nums= new int[n];
-        for (int i =0;i<nums.length ;i++){
-            nums[i] = sc.nextInt();
+        int n = sc.nextInt();
+        int [] nums= new int[n];
+        for (int i=0;i< nums.length;i++){
+            nums[i]= sc.nextInt();
         }
-
         sc.close();
-
         TreeNode head = arrtotree(nums);
-        ArrayList<Integer> result = preorderTreeNode(head);
-        for (int num:result){
+        ArrayList<Integer> result =postorderTraversal(head);
+        for (int num: result){
             System.out.print(num+ " ");
         }
         System.out.println();
     }
-    public static TreeNode arrtotree(int [] nums){
+
+    public  static TreeNode arrtotree(int [] nums){
         if (nums.length ==0){
             return null;
         }
 
-        Queue<TreeNode> queue= new LinkedList<>();
-        TreeNode head = new TreeNode(nums[0]);
+        Queue <TreeNode> queue = new LinkedList<>();
 
+        TreeNode head = new TreeNode (nums[0]);
         queue.offer(head);
 
         int i =1;
@@ -45,26 +47,25 @@ class main{
                 curr.left = new TreeNode(nums[i++]);
                 queue.offer(curr.left);
             }
-            if (i< nums.length){
-                curr.right = new TreeNode (nums[i++]);
+            if (i< nums.length ){
+                curr.right = new TreeNode(nums[i++]);
                 queue.offer(curr.right);
             }
         }
         return head;
     }
-
-    public static ArrayList<Integer> preorderTreeNode(TreeNode head){
+    public static ArrayList<Integer> postorderTraversal(TreeNode head){
         ArrayList<Integer> ans = new ArrayList<>();
-        preorder(head,ans);
+        postorder(head, ans);
         return ans;
     }
-    public static void preorder (TreeNode head, ArrayList<Integer> ans){
-        if (head == null){
-            return ;
+    public static void postorder(TreeNode head, ArrayList<Integer> ans){
+        if (head== null){
+            return;
         }
+
+        postorder(head.left, ans);
+        postorder(head.right, ans);
         ans.add(head.data);
-        preorder(head.left, ans);
-        preorder(head.right, ans);
-        
     }
 }
